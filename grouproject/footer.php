ae4f7651ae572f805cfeaa9f21f04c6a22944126
cramirez3182 <cramirezborrego@scu.edu>
2026-06-10 17:57:05 -0700

    Updated footer.php
    Fixed footer formatting

--- grouproject/footer.php
+++ grouproject/footer.php
@@ -4,7 +4,7 @@
         <footer><!--php footer?-->
             <script>
                 const currentYear = new Date().getFullYear();
-                document.write("&copy "+currentYear + "Mitigation of Misinformation - EPIC Labs");
+                document.write("&copy "+currentYear + " Mitigation of Misinformation - EPIC Labs");
             </script>
 
 
