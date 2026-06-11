f7aefba0197a3f0240ee55e87c99651865b221fa
cramirez3182 <cramirezborrego@scu.edu>
2026-06-10 17:56:25 -0700

    Updated footer.php
    Updated footer.php to include copyright symbol and MoM and labs name

--- grouproject/footer.php
+++ grouproject/footer.php
@@ -4,7 +4,7 @@
         <footer><!--php footer?-->
             <script>
                 const currentYear = new Date().getFullYear();
-                document.write("copyright @ "+currentYear);
+                document.write("&copy "+currentYear + "Mitigation of Misinformation - EPIC Labs");
             </script>
 
 
