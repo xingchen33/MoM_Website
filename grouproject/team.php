d1a167582dbc1d9372a70b9f9576f2b86ebe5f2f
cramirez3182 <cramirezborrego@scu.edu>
2026-07-27 14:50:54 -0700

    Updated team.php to add profile pictures
    Added an img element to each profile card that pulls from each person's linkedin profiles. People who don't have one get a default no profile image picture. Images are rounded with edges and have a margin. The only one who's different is the prof's which pulls from his github profile picture.

--- grouproject/team.php
+++ grouproject/team.php
@@ -27,7 +27,7 @@
 			<h1>Meet the Team</h1>
 			<p>
 				See who the faculty advisor, research team, and website developers
-				who created this site are.
+				who created this site are!
 			</p>
 		</div>
 
@@ -38,6 +38,11 @@
 					Faculty Advisor</h2>
 				<div class="containerOuter">
 					<div class="container">
+
+						<!-- LinkedIn Profile picture -->
+
+						<img src="https://avatars.githubusercontent.com/u/37026687?v=4" alt="Headshot of Navid." style="width: 200px; height: 200px; border-radius: 12px; margin: 15px;"/>
+
 						<h3>Navid Shaghaghi</h3>
 						<hr/>
 						<h4 class="webdesc"></h4>
@@ -60,6 +65,11 @@
 					Research Team</h2>
 				<div class="containerOuter">
 					<div class="container">
+
+						<!-- LinkedIn Profile picture -->
+
+						<img src="https://media.licdn.com/dms/image/v2/D5603AQH-fIN2a4mTOg/profile-displayphoto-crop_800_800/B56Z2s8g_DKwAU-/0/1776723044594?e=1786579200&v=beta&t=UoSP81iYTbQMdZLn9yxi2-HpVPrlhTA3ggB23voBJfM" alt="Headshot of Heather." style="width: 200px; height: 200px; border-radius: 12px; margin: 15px;"/>
+
 						<h3>Heather Lyons</h3>
 						<hr/>
 						<h4 class="webdesc"></h4>
@@ -76,6 +86,11 @@
 					</div>
 
 					<div class="container">
+
+						<!-- LinkedIn No profile picture icon -->
+
+						<img src="https://media.licdn.com/dms/image/v2/D4E12AQEud3Ll5MI7cQ/article-inline_image-shrink_1500_2232/article-inline_image-shrink_1500_2232/0/1660833954461?e=1786579200&v=beta&t=09WdTl7W47_tHDfkzLWQBQy2_PInANs2vrb9NHGCQfE" alt="Headshot of Edgar." style="width: 200px; height: 200px; border-radius: 12px; margin: 15px;"/>
+
 						<h3>Edgar Garcia</h3>
 						<hr/>
 						<h4 class="webdesc"></h4>
@@ -92,6 +107,11 @@
 					</div>
 
 					<div class="container">
+
+						<!-- LinkedIn Profile picture -->
+
+						<img src="https://media.licdn.com/dms/image/v2/D5603AQFTr1agrMy3yQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1709079520759?e=1786579200&v=beta&t=F2qxtSzesyQKwC8iUJM9C0kKOBg_u2vLe1UjW7stPbA" alt="Headshot of Isabel." style="width: 200px; height: 200px; border-radius: 12px; margin: 15px;"/>
+
 						<h3>Isabel Zuluaga Gonzalez</h3>
 						<hr/>
 						<h4 class="webdesc"></h4>
@@ -116,6 +136,11 @@
 			<section class="section split3" id="team">
 				<div class="containerOuter">
 					<div class="container">
+
+						<!-- LinkedIn Profile picture -->
+
+						<img src="https://media.licdn.com/dms/image/v2/D5603AQGJMM2rVFwxhQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1721627415817?e=1786579200&v=beta&t=HSauEuZTAQgZ9utz3yedOJzq4ab9cVE7u8NzKf8sHQw" alt="Headshot of Samantha." style="width: 200px; height: 200px; border-radius: 12px; margin: 15px;"/>
+
 						<h3>Samantha Lee</h3>
 						<hr/>
 						<h4 class="webdesc"></h4>
@@ -132,6 +157,11 @@
 					</div>
 
 					<div class="container">
+
+						<!-- LinkedIn No profile picture icon -->
+
+						<img src="https://media.licdn.com/dms/image/v2/D4E12AQEud3Ll5MI7cQ/article-inline_image-shrink_1500_2232/article-inline_image-shrink_1500_2232/0/1660833954461?e=1786579200&v=beta&t=09WdTl7W47_tHDfkzLWQBQy2_PInANs2vrb9NHGCQfE" alt="Headshot of Jazmin." style="width: 200px; height: 200px; border-radius: 12px; margin: 15px;"/>
+
 						<h3>Jazmin Sanchez</h3>
 						<hr/>
 						<h4 class="webdesc"></h4>
@@ -148,6 +178,11 @@
 					</div>
 
 					<div class="container">
+
+						<!-- LinkedIn No profile picture icon -->
+
+						<img src="https://media.licdn.com/dms/image/v2/D4E12AQEud3Ll5MI7cQ/article-inline_image-shrink_1500_2232/article-inline_image-shrink_1500_2232/0/1660833954461?e=1786579200&v=beta&t=09WdTl7W47_tHDfkzLWQBQy2_PInANs2vrb9NHGCQfE" alt="Headshot of Roberto." style="width: 200px; height: 200px; border-radius: 12px; margin: 15px;"/>
+
 						<h3>Roberto Hincapie</h3>
 						<hr/>
 						<h4 class="webdesc"></h4>
@@ -171,6 +206,11 @@
 				<div class="containerOuter">
 
 					<div class="container">
+
+						<!-- LinkedIn Profile picture -->
+
+						<img src="https://media.licdn.com/dms/image/v2/D5603AQGhwrDmlZGxqQ/profile-displayphoto-crop_800_800/B56Z24KWGjH4AM-/0/1776911213906?e=1786579200&v=beta&t=_BVrpH2yWFAHG4ZFeEtw389FmBz9oaI4O2Jbz9H-35U" alt="Yue's profile picture." style="width: 200px; height: 200px; border-radius: 12px; margin: 15px;"/>
+
 						<h3>Yue Yu</h3>
 						<hr/>
 						<h4 class="webdesc"></h4>
@@ -187,6 +227,11 @@
 					</div>
 
 					<div class="container">
+
+						<!-- LinkedIn Profile picture -->
+
+						<img src="https://media.licdn.com/dms/image/v2/D4E03AQEYDjOPEf5Jgg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1707589508310?e=1786579200&v=beta&t=_43zMeKXKwj7o_GElrrHl4Vg54s86Sbfp63fnxcE6RU" alt="Headshot of Cuitlahuac." style="width: 200px; height: 200px; border-radius: 12px; margin: 15px;"/>
+
 						<h3>Cuitlahuac Ramirez</h3>
 						<hr/>
 						<h4 class="webdesc"></h4>
@@ -207,6 +252,12 @@
 					</div>
 
 					<div class="container">
+
+						<!-- LinkedIn Profile picture -->
+
+						<img src="https://media.licdn.com/dms/image/v2/C5603AQE27S9CRD7uYw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1652389435703?e=1786579200&v=beta&t=knWofTaqrba7A4N1do2FlXxcGo3HdorKVy5iZ6Cjk1k" alt="Headshot of Emmet." style="width: 200px; height: 200px; border-radius: 12px; margin: 15px;"/>
+
+
 						<h3>Emmet Whitehead</h3>
 						<hr/>
 						<h4 class="webdesc"></h4>
