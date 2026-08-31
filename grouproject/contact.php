1e4cf42c2b20aef9ac086e0e5a72f9175f593244
cramirez3182 <cramirezborrego@scu.edu>
2026-08-31 16:25:15 -0700

    Updated site UI
    Added new design features and revamped some page designs for better user experience

--- grouproject/contact.php
+++ grouproject/contact.php
@@ -1,192 +1,223 @@
 <html>
     <head>
         <title>Contact Us</title>
+
         <link rel="stylesheet" href="css/base.css">
         <link rel="stylesheet" href="css/contact.css">
+
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
-         <!-- Loads the hCaptcha script so the captcha box can appear on the page next to the form -->
+
+        <!-- Loads the hCaptcha script so the captcha box can appear -->
         <script src="https://js.hcaptcha.com/1/api.js" async defer></script>
 
-        <!-- favicon link -->
+        <!-- favicon -->
         <link rel="icon" type="image/favicon" href="img/favicon.ico">
     </head>
 
     <body>
+
         <?php include "nav.php";?>
-        <div id='content'>
-            <div id='form'>
-                <h1>Contact Us!</h1>
+
+        <div id="content">
+
+            <div id="contact-left">
+
+                <h1>Contact Us</h1>
+
                 <p>Fill out this form to email us!</p>
+
+                <img src="img/contactbackground.png" alt="Envelope illustration">
+
+            </div>
+
+            <div id="contact-right">
 
                 <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 
-                <div id="short">
-                    <label for ="name">Name <span>*</span></label>
-                    <input type = "text" id ="name" name="name" required/>
-                    
-                    <label for = "email">Email <span>*</span></label>
-                    <input type = "text" id="email" name="email" required/>
+                    <div id="short">
 
-                </div>
+                        <div class="short-field">
 
-                    <label for = "message">Message <span>*</span></label>
-                    <textarea type = "text" id="message" name="message" required> </textarea>
+                            <label for="name">
+                                Name <span>*</span>
+                            </label>
 
-                <div id="cap">
+                            <input type="text" id="name" name="name" required>
+                        </div>
 
-                    <!-- Connects to my hCaptcha account -->
-                    <div class="h-captcha" data-sitekey="0f804a9e-76e4-41f4-b038-7430ef6217de"></div>
-                
-                    <button class= 'internal' type = 'submit' id='submit'> submit </button>
 
-                </div>
+                        <div class="short-field">
+
+                            <label for="email">
+                                Email <span>*</span>
+                            </label>
+
+                            <input type="text" id="email" name="email" required>
+                        </div>
+                    </div>
+
+                    <label for="message">
+                        Message <span>*</span>
+                    </label>
+
+                    <textarea id="message" name="message" required>
+                    </textarea>
+
+                    <div id="cap">
+
+                        <!-- Connects to my hCaptcha account -->
+                        <div
+                            class="h-captcha" data-sitekey="0f804a9e-76e4-41f4-b038-7430ef6217de">
+                        </div>
+
+                        <button class="internal" type="submit" id="submit"> Send!
+                        </button>
+
+                    </div>
                 </form>
             </div>
         </div>
 
-    <?php
+        <?php
 
-        function clean($data){
-            $data = trim($data);
-            $data = stripcslashes($data);
-            return $data;
+            function clean($data){
+                $data = trim($data);
+                $data = stripcslashes($data);
+                return $data;
+            }
 
-        }
-
-        $name = '';
-        $email = '';
-        $message = '';
-
-        if($_SERVER["REQUEST_METHOD"]=="POST"){
-
-            $name = clean ($_POST["name"] ?? '');
-            $email = clean($_POST["email"] ?? '');//get the post data from form
-            $message = clean($_POST["message"] ?? '');
-
-            // After the captcha is completed by the user, a value gets created
-            // but if the user doesn't complete the captcha, this value is empty
-            $captchaResponse = $_POST["h-captcha-response"] ?? '';
-            // key unique to the hcaptcha account I made
-                        $secretKey = "ES_d14c5aa2c7af4f2d85fda6e3ea9aa608";
-            
-            if($name !== '' && $email !== '' && $message !== ''){
-
-                // checks that the catpcha was done
-                if($captchaResponse == ''){
-                    echo "<script>
-                            alert('please complete captcha!');
-                            window.location.href = window.location.href;
-                            
-                        </script>";
-                    exit;
-                }
-
-                // the URL to verify the catpcha
-                $verifyURL = "https://hcaptcha.com/siteverify";
-
-                // this is the data that'll get sent to hCaptcha
-                $data = [
-                    "secret" => $secretKey,
-                    "response" => $captchaResponse
-                ];
+            $name = '';
+            $email = '';
+            $message = '';
 
 
-                // the settings for the post request sent to hCaptcha
-                $options = [
-                    "http" => [
-                        "header" => "Content-type: application/x-www-form-urlencoded\r\n",
+            if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+                $name = clean($_POST["name"] ?? '');
+                $email = clean($_POST["email"] ?? '');
+                $message = clean($_POST["message"] ?? '');
+
+
+                // After the captcha is completed by the user,
+                // a value gets created
+                $captchaResponse = $_POST["h-captcha-response"] ?? '';
+
+
+                // key unique to the hCaptcha account
+                $secretKey = "ES_d14c5aa2c7af4f2d85fda6e3ea9aa608";
+
+
+                if($name !== '' && $email !== '' && $message !== ''){
+
+
+                    // checks that the captcha was done
+                    if($captchaResponse == ''){
+
+                        echo "<script>
+                                alert('please complete captcha!');
+                                window.location.href = window.location.href;
+                              </script>";
+
+                        exit;
+                    }
+
+                    // URL used to verify captcha
+                    $verifyURL = "https://hcaptcha.com/siteverify";
+
+                    // data sent to hCaptcha
+                    $data = [
+                        "secret" => $secretKey,
+                        "response" => $captchaResponse
+                    ];
+
+                    // settings for request sent to hCaptcha
+                    $options = [
+                        "http" => ["header" =>"Content-type: application/x-www-form-urlencoded\r\n",
                         "method" => "POST",
                         "content" => http_build_query($data)
                     ]
-                ];
+                    ];
 
-                // Sends the verification request to hCaptcha and decode the response
-                $context = stream_context_create($options);
-                $result = file_get_contents($verifyURL, false, $context);
-                $responseData = json_decode($result);
 
-                // key part: if the captcha response is valid, then the email is good to send
-                if($responseData && $responseData->success){
-                    
-                    // $recipient = "noreply@scu.edu";//can be changed to other address
-                    $subject = "New Contact Form Message from $name: \n\n";
+                    // sends verification request
+                    $context = stream_context_create($options);
 
-                    $body = "You have recieved a new message from the MoM website contact
-                    form. \n\n".
-                    "Name: $name\n".
-                    "Email: $email\n".
-                    "Message: \n$message";
+                    $result = file_get_contents($verifyURL, false, $context);
 
-                        //write sample email
+                    $responseData = json_decode($result);
 
-                    $baseName = "sample";
-                    $extension = ".txt";
+                    // if captcha response is valid
+                    if($responseData && $responseData->success){
+                        $subject ="New Contact Form Message from $name: \n\n";
 
-                    $filename = $baseName . $extension;
-                    $counter = 1;
+                        $body =
+                            "You have received a new message from the MoM website contact form. \n\n" .
+                            "Name: $name\n" . "Email: $email\n" . "Message: \n$message";
 
-                    while (file_exists($filename)) {
-                        $filename = $baseName . "(" . $counter . ")" . $extension;
-                        $counter++;
+                        // writes sample email
+
+                        $baseName = "sample";
+
+                        $extension = ".txt";
+
+                        $filename = $baseName . $extension;
+
+                        $counter = 1;
+
+                        while(file_exists($filename)){
+
+                            $filename = $baseName . "(" . $counter . ")" .
+                            $extension;
+
+                            $counter++;
+                        }
+
+                        $output =
+                            fopen($filename, "w")
+                            or die("File not found");
+
+                        if(fwrite($output, $subject . $body)){
+
+                            echo "<script>
+                                    alert('Message saved successfully!');
+                                    window.location.href = window.location.href;
+                                  </script>";
+                        }
+
+                        else{
+
+                            echo "<script>
+                                    alert('Something went wrong');
+                                    window.location.href = window.location.href;
+                                  </script>";
+                        }
+
+                        fclose($output);
+                        exit;
                     }
 
-                    $output = fopen($filename, "w")or die("File not found");
-
-                    if (fwrite($output, $subject . $body)) {
+                    else{
                         echo "<script>
-                            alert('Message saved successfully!');
-                            window.location.href = window.location.href;
-                        </script>";
-                    } else {
-                        echo "<script>alert('Something went wrong');
-                        window.location.href = window.location.href;</script>";
+                                alert('captcha failed...');
+                                window.location.href = window.location.href;
+                              </script>";
                     }
+                }
+                else{
 
-                    // fwrite($output, $subject);
-                    // fwrite($output,$body);
-                    // $headers = "From: noreply@ssl.students.engr.scu.edu\r\n";
-                    // $headers .= "Reply-To: $email\r\n";
-                    // $headers .="X-Mailer : PHP/" . phpversion();
+                    echo "<script>
+                            alert('Required fields cannot be empty!');
+                            window.location.href = 'contact.php';
+                          </script>";
 
-
-
-                    // if(mail($recipient, $subject, $body, $headers)){
-                    //     echo "Your message has been sent!";
-                    // } else {
-                    //     echo "Oh no...Something went wrong.";
-                    // }
-
-                    fclose($output);
                     exit;
-
-            }else{
-                echo "<script>
-                    alert('captcha failed...');
-                    window.location.href = window.location.href;
-                            
-                </script>";
+                }
             }
+        ?>
 
-            }else{
-                echo "<script>
-                    alert('Required fields cannot be empty!');
-                     window.location.href = 'contact.php';
-                            
-                </script>";
-                exit;
-            }
+        <?php include "footer.php";?>
 
-
-        }
-    
-
-
-    ?>
-
-    <?php include "footer.php";?>
-
-
-        </body>
+    </body>
 </html>
--- grouproject/home.php
+++ grouproject/home.php
@@ -7,6 +7,8 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
 
+        <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+
         <!-- favicon link -->
         <link rel="icon" type="image/favicon" href="img/favicon.ico">
     </head>
@@ -17,12 +19,11 @@
         
             <div id="cover">
                 <div class="left">
-                    <h1>Welcome to the Mitigation of Misinformation Project! (MoM)</h1>
-                    <p>Asking the question... what would MoM do?
-                    </p>
+                    <h1>Mitigation of Misinformation (MoM) <br> Research Page</h1>
+                    <p>Asking the question... what would MoM do?</p>
                 </div>
                 <aside class="right">
-                    <img src="img/home_cover.png" alt="Illustration of a person next to an abnormally large cell phone."/>
+                    <!-- <img src="img/home_cover.png" alt="Illustration of a person next to an abnormally large cell phone."/> -->
                 </aside>
             
             </div>
@@ -37,12 +38,12 @@
 
                     <div class="left">
                         <img src="img/home_why.png" alt="Illustration of a woman walking a dog looking down at their cell phone."/>
-                        <a class="external" href="external/poster.pdf", target="_blank">View the Research Poster</a>
+                        <!-- <a class="external" href="external/poster.pdf", target="_blank">View the Research Poster</a> -->
                     </div>
                     <aside class="right">
                         <ul>
-                            <li><img class="why_svg" src="img/why_1.png" alt="An illustration of a triangular hazard sign with an exclamation mark in the center.">The spread of misinformation is running rampant on social media</li>
-                            <li><img class="why_svg" src="img/why_2.png" alt="Illustration a brain with a qustion mark in the center.">It's becoming increasingly difficult to discern what is fake and not</li>
+                            <li><img class="why_svg" src="img/why_1.png" alt="An illustration of a triangular hazard sign with an exclamation mark in the center.">The spread of misinformation is running rampant on social media!</li>
+                            <li><img class="why_svg" src="img/why_2.png" alt="Illustration a brain with a qustion mark in the center.">It's becoming increasingly difficult to discern what is fake and not!</li>
                             <li><img class="why_svg" src="img/why_3.png" alt="Illustration of a magnifying glass with a checkmark in the middle.">There has to be a better way to figure out what is misinformation and what is not!</li>
                             <!-- <li id="poster"> <- Click the link to learn more from the research poster!</li> -->
 
@@ -70,45 +71,55 @@
                     </li>
                 </ul>
 
-                <a id="pipeline" class="external" href="external/pipeline.png", target = "_blank">View our Pipeline to learn more!</a>
+                <!-- <a id="pipeline" class="external" href="external/pipeline.png", target = "_blank">View our Pipeline to learn more!</a> -->
 
             </div>
 
             <div class='section' id="you">
                 <h2>What You Can Do</h2>
-                <div class = content>
-
-                    <div class="left">
-                        <!-- <a class="external" href="#">Check out the project's main site</a> -->
-                        <div class="line">
-                            <p>Read our publication!</p>
-                            <a class="internal" href="https://link.springer.com/chapter/10.1007/978-3-031-33614-0_13", target="_blank">Learn more!</a>
-                        </div>
-                        <div class="line">
-                            <p>Meet the team!</p>
-                            <a class="internal" href="team.php">Learn more!</a>
-                        </div>
-                    </div>
-                
-                    <aside id="game" class="right">
-                        
-                        <aside class="left">
-                            <img src="img/home_game.png" alt="Image of a person with a virtual reality headset, arms extended, and icons in orbs floating around him; feline at his feet."/>
-                        </aside>
-                        
-                        <div class="right">
-                            <p id="gameTitle">Misinformation Article Minigame
-                            <p>Play this minigame to test your classification skills!</p>
-                            <a class="internal" href="game.php">Play Mini-game</a>
+                <div class="you-options">
+                    <div class="you-card">
+                        <div class="icon-circle">
+                            <img src="img/home_1.png" alt="Pipeline icon.">
                         </div>
 
+                        <a href="pipeline.php">View The Pipeline</a>
+                        
+                        <p>Learn how the project works!</p>
                     </div>
 
+                    <div class="you-card">
+                        <div class="icon-circle">
+                            <img src="img/home_2.png" alt="Developer icon.">
+                        </div>
+
+                        <a href="team.php">Meet the Team</a>
+                        
+                        <p>Check out the people working on this team!</p>
+                    </div>
+                        <div class="you-card">
+                            <div class="icon-circle">
+                                <img src="img/home_3.png" alt="Contact icon.">
+                            </div>
+
+                            <a href="contact.php">Contact Us</a>
+                            
+                            <p>Any questions?<br>Fill out our contact form!</p>
+                        </div>
+                    </div>
                 </div>
-
-
             </div>
         </main>
+
+        <div class="bottom-banner">
+            <img src="img/paper_banner.png" alt="Paper illustration.">
+
+            <div class="banner-text">
+                <h2>Read Our Research Paper!</h2>
+                    <!-- <p></p> -->
+                <a href="https://link.springer.com/chapter/10.1007/978-3-031-33614-0_13" target="_blank">Detecting Fake News Spreaders on Twitter Through Follower Networks - published May, 2023</a>
+            </div>
+        </div>
 
         <?php include "footer.php";?>
 
